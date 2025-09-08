5eaf1bc8d985aea619af343c4082cbdffa1da2df
anton.solobutov@greenice.net <anton.solobutov@greenice.net>
2025-09-08 19:48:47 +0200

    TLS: speed page plugin

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const jakarta = Plus_Jakarta_Sans({
   variable: "--font-jakarta",
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body
         className={`${jakarta.variable} antialiased`}
       >
+        <SpeedInsights/>
         <Header />
         {children}
         <Footer />
