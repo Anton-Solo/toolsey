1b0b6c351042f181f690d6ae7b17066857368de2
anton.solobutov@greenice.net <anton.solobutov@greenice.net>
2025-11-20 11:20:24 +0100

    TLS: meta pixel and google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,9 +43,52 @@ export default function RootLayout({
           strategy="beforeInteractive"
         />
         <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-G94B9NKK9Q"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-G94B9NKK9Q');
+            `,
+          }}
+        />
+        <Script
           src="https://www.google.com/recaptcha/api.js"
           strategy="afterInteractive"
         />
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '357346202853059');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=357346202853059&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body
         className={`${jakarta.variable} antialiased`}
